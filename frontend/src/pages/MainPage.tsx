cb348db8221a688c00faf12475fa4adf4591efe6
jin <jinspace3710@gmail.com>
2022-03-14 11:46:27 +0900

    Design : 메인 페이지 마크업 및 스타일링 구현

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,14 +1,18 @@
 import { useEffect } from 'react';
+
 import Footer from '../components/common/Footer';
 import Header from '../components/common/Header';
+import MainBannerSection from '../components/main/MainBannerSection';
 
 const MainPage: React.FC = () => {
   useEffect(() => {
     document.title = '싸핏';
   }, []);
+
   return (
     <>
       <Header />
+      <MainBannerSection />
       <Footer />
     </>
   );
