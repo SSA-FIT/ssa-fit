d49913ab6da7548e8c5f9ad182cb7f805477a1dc
dudqo225 <dudqo225@gmail.com>
2022-03-29 17:01:01 +0900

    Feat : 운동 관련 Entity 생성 및 모델링

---
+++ backend/spring/src/main/java/com/ssafy/ssafit/entity/ExerciseHistory.java
@@ -0,0 +1,45 @@
+package com.ssafy.ssafit.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExerciseHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int countPerSet;
+
+    private int set;
+
+    @Column(length = 45)
+    private String durationTime;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(targetEntity = Exercise.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "exercise_id")
+    private Exercise exercise;
+}
