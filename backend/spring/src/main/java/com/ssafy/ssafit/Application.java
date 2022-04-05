1200337214edcb3d068085330306fc1662bc1c52
Jo-wonbin <jwb7214@gmail.com>
2022-04-05 10:57:36 +0900

    Feat : 서버 시간 한국시간으로 변경

--- backend/spring/src/main/java/com/ssafy/ssafit/Application.java
+++ backend/spring/src/main/java/com/ssafy/ssafit/Application.java
@@ -3,8 +3,17 @@ package com.ssafy.ssafit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
